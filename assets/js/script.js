f8fee834f289b6adf7218dc10e909dbefd506fa2
imsorayat <sorayataraszka@gmail.com>
2024-06-22 17:21:42 +0100

    remove the scroll effect on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,28 @@
 $(window).on('load', function(){
-    // var distance = $('.snap').position();
-
-    // $(window).scroll(function() {
-    //     console.log(distance);
-    //     // if ( $(this).scrollTop() >= distance ) {
-    //     //     console.log('is in top');
-    //     // } else {
-    //     //     console.log('is not in top');
-    //     // }
-    // });
-
     
-    var controller = new ScrollMagic.Controller();
+    let viewPortWidth = $(window).width();
 
-    $('.slide').css('opacity', 0);
-    new ScrollMagic.Scene({
-        reverse: true,
-        duration: 400,
-        triggerHook: "onCenter",
-        offset: 400,
-    })
-        .triggerElement('#slide1') // pins the element for the the scene's duration
-        .setPin('#slide1')
-        .on('start', function(){
-            $('#slide1').animate({'opacity': 1});
-        })
-        .on("end", function(){
-            $('#slide1').animate({'opacity': 0});
-            
-        })
-        .addTo(controller);
+    if(viewPortWidth > 900){
+        console.log(viewPortWidth);
+        var controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        reverse: true,
-        duration: 400,
-        offset: 400,
-        triggerHook: "onCenter",
-    })
-        .triggerElement('#slide2') // pins the element for the the scene's duration
-        .setPin('#slide2')
-        .on('start', function(){
-            $('#slide2').animate({'opacity': 1});
+        $('.slide').css('opacity', 0);
+        new ScrollMagic.Scene({
+            reverse: true,
+            duration: 400,
+            triggerHook: "onCenter",
+            offset: 400,
         })
-        .on("end", function(){
-            $('#slide2').animate({'opacity': 0});
-            
-        })
-        .addTo(controller);
+            .triggerElement('#slide1') // pins the element for the the scene's duration
+            .setPin('#slide1')
+            .on('start', function(){
+                $('#slide1').animate({'opacity': 1});
+            })
+            .on("end", function(){
+                $('#slide1').animate({'opacity': 0});
+                
+            })
+            .addTo(controller);
 
         new ScrollMagic.Scene({
             reverse: true,
@@ -54,16 +30,33 @@ $(window).on('load', function(){
             offset: 400,
             triggerHook: "onCenter",
         })
-            .triggerElement('#slide3') // pins the element for the the scene's duration
-            .setPin('#slide3')
+            .triggerElement('#slide2') // pins the element for the the scene's duration
+            .setPin('#slide2')
             .on('start', function(){
-                $('#slide3').animate({'opacity': 1});
+                $('#slide2').animate({'opacity': 1});
             })
             .on("end", function(){
-                $('#slide3').animate({'opacity': 0});
+                $('#slide2').animate({'opacity': 0});
                 
             })
             .addTo(controller);
+
+            new ScrollMagic.Scene({
+                reverse: true,
+                duration: 400,
+                offset: 400,
+                triggerHook: "onCenter",
+            })
+                .triggerElement('#slide3') // pins the element for the the scene's duration
+                .setPin('#slide3')
+                .on('start', function(){
+                    $('#slide3').animate({'opacity': 1});
+                })
+                .on("end", function(){
+                    $('#slide3').animate({'opacity': 0});
+                    
+                })
+                .addTo(controller);
     
-   
+        }
 });
